7df3b5d525d7e47cb39e8f1a5442c20148766110
Floriano <floriano@email.com>
2026-09-16 20:58:28 -0300

    Inicio da criação da parte usuarios do sistema

---
+++ src/repositories/usersRepository.js
@@ -0,0 +1,26 @@
+import pool from '../configs/database.js';
+
+const usersRepository = {
+    criar: async (nome, email, senha_hash, tipo_usuario) =>{
+        const sql = "INSERT INTO usuarios (nome, email, senha_hash, tipo_usuario) VALUES (null, ?, ?, ?, ?, null);";
+        const [result] = await pool.execute(sql, [nome, email, senha_hash, tipo_usuario]);
+        return result;
+    },
+    encontrar: async () =>{
+        const sql = "SELECT * FROM usuarios;";
+        const [result] = await pool.execute(sql);
+        return result;
+    },
+    encontrarPorId: async (id) =>{
+        const sql = "SELECT FROM user WHERE id = ?";
+        const [result] = await pool.execute(sql, [id]);
+        return result;
+    },
+    atualizar: async (nome, email, senha_hash, tipo_usuario) =>{
+        const sql = "UPDATE usuarios SET nome = ?, email = ?, senha_hash = ?, tipo_usuario = ? WHERE id = ?"
+        const [result] = await pool.execute(sql, [nome, email, senha_hash, tipo_usuario]);
+        return result;
+    }
+};
+
+export default usersRepository;
