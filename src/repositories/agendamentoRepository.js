a07520c43c38a01b097d2c210b73e47e64fba938
Floriano <floriano@email.com>
2026-09-23 21:14:30 -0300

    começando o agendamento

---
+++ src/repositories/agendamentoRepository.js
@@ -0,0 +1,9 @@
+import pool from '../configs/database.js';
+
+const agendamentoRepository = {
+    criar: async () =>{
+        const sql = "INSERT INTO agendamentos VALUES ();";
+    }
+}
+
+export default agendamentoRepository;
