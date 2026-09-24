0d4eb857e3bd0a5424c7b3350797ca29c2298d62
Floriano <floriano@email.com>
2026-09-23 21:37:56 -0300

    criando repositorie

--- src/repositories/agendamentoRepository.js
+++ src/repositories/agendamentoRepository.js
@@ -1,9 +1,23 @@
 import pool from '../configs/database.js';
 
 const agendamentoRepository = {
-    criar: async () =>{
-        const sql = "INSERT INTO agendamentos VALUES ();";
-    }
+    criar: async (id_passageiro, id_voo, data_agendamento, agendamentoStatus) =>{
+        const sql = "INSERT INTO agendamentos (id_passageiro, id_voo, data_agendamento, agendamentoStatus)VALUES (?,?,?,?);";
+        const [result] = await pool.execute(sql, [id_passageiro, id_voo, data_agendamento, agendamentoStatus]);
+        return result;
+    },
+    listarTudo: async () =>{
+        const sql = "SELECT * FROM agendamentos;";
+        const [result] = await pool.execute(sql);
+        return result;
+    },
+    listarId: async (id_agendamento) => {
+        const sql = "SELECT FROM agendamentos WHERE id_passageiro = ?;";
+        const [result] = await pool.execute(sql, [id_agendamento]);
+        return result;
+    },
+    
+
 }
 
 export default agendamentoRepository;
